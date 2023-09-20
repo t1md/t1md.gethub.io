7784c63483507b8ef8694ad08d71438181c63d4b
t1md <145151107+t1md@users.noreply.github.com>
2023-09-20 12:48:44 -0600

    finished

--- primitive paint/sketch.js
+++ primitive paint/sketch.js
@@ -1,45 +1,53 @@
 // Primitive Paint
 // Tim Dobson
 // 9/15/2023
+//draws shapes either square,circle,triangle
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// i made it preview shape and change size according to mouse
 
-let shapeheight,shapewidth;
+
+//global variables
+let shapeheight=100,shapewidth=100;
 let overlay;
 let shape;
 let circleX, circleY;
-let xmove,ymove;
-let rad;
+let xmove=0.05,ymove=0.05;
+let rad = 0;
 let bgcolor = 255;
 let fr, fg, fb, sr ,sg ,sb ;
 let currentkey;
 
 function setup() {
+  //sets up and creats all the constants
   createCanvas(windowWidth, windowHeight);
   overlay = createGraphics(windowWidth,windowHeight);
   overlay.rectMode(CENTER);
   rectMode(CENTER);
   circleX = mouseX, circleY=mouseY;
-  xmove=0.05,ymove=0.05;
-  rad = 0;
-  shapeheight=100, shapewidth=100;
   overlay.fill(0);
+  textFont("Brush Script MT");
+  textSize(20);
 }
 
 function draw() {
+  //draws the items and constantly updates the overlay
   background(bgcolor);
   image(overlay,0,0);
   rotatingCircle();
   preview();
   myname();
 }
+
 function myname(){
+  //rights my name in bottom right
   fill(0);
   stroke(0);
-  text("Timothy Dobson",width*0.84,height*0.99);
+  text("Timothy Dobson",width*0.85,height*0.99);
 }
+
 function rotatingCircle(){
+  //rotats a circle around the cursor
   rad += xmove;
   circleX = mouseX + Math.cos(rad)*100;
   circleY = mouseY + Math.sin(rad)*100;
@@ -47,7 +55,9 @@ function rotatingCircle(){
   stroke(sr,sg,sb);
   ellipse(circleX,circleY,30,30 );
 }
+
 function preview(){
+  //draws a preview of the shape
   if (currentkey === "a"){
     rect(mouseX,mouseY,shapewidth,shapeheight);
   }
@@ -58,26 +68,22 @@ function preview(){
     triangle(mouseX,mouseY-shapeheight/2,mouseX-shapewidth/2,mouseY
     +shapeheight/2,mouseX+shapewidth/2,mouseY+shapeheight/2);
   }
-
 }
-function mousePressed(){
+
+function keyPressed(){
+  //takes in key presses and tells what key was pressed for the drawing
+  //changes colours or resets the screen
   if(key === "a"){
-    overlay.rect(mouseX,mouseY,shapewidth,shapeheight);
-    image(overlay,0,0);
     currentkey = "a";
   }
   if(key === "s"){
-    overlay.ellipse(mouseX,mouseY,shapewidth,shapeheight);
-    image(overlay,0,0);
     currentkey = "s";
   }
   if(key === "d"){
-    overlay.triangle(mouseX,mouseY-shapeheight/2,mouseX-shapewidth/2,mouseY
-    +shapeheight/2,mouseX+shapewidth/2,mouseY+shapeheight/2);
-    image(overlay,0,0);
     currentkey = "d";
   }
   if(key === "c"){
+    //changes colour to a random colour
     let r = random(256);
     let g = random(256);
     let b = random(256);
@@ -92,10 +98,38 @@ function mousePressed(){
     sr=r,sg=g,sb=b;
   }
   if (key ===" "){
+    //clean slates 
     overlay.background(bgcolor);
     image(overlay,0,0);
   }
   else{
     console.log(key);
+  }
+}
+
+function mousePressed(){
+  //draws a shape depending on what key had been pressed upon pressing the mouse
+  if (currentkey === "a"){
+    overlay.rect(mouseX,mouseY,shapewidth,shapeheight);
+  }
+  if (currentkey === "s"){
+    overlay.ellipse(mouseX,mouseY,shapewidth,shapeheight);
+  }
+  if (currentkey === "d"){
+    overlay.triangle(mouseX,mouseY-shapeheight/2,mouseX-shapewidth/2,mouseY
+    +shapeheight/2,mouseX+shapewidth/2,mouseY+shapeheight/2);
+  }
+
+}
+
+function mouseWheel(event){
+  //changes the size of the shape when you scroll your scrollwheel
+  if(event.delta>0){
+    shapewidth+=5;
+    shapeheight+=5;
+  }
+  if(event.delta<0){
+    shapewidth-=5;
+    shapeheight-=5;
   }
 }
