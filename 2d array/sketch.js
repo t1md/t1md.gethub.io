e31d4107e7cab59fb37d6146677b88675bee6318
t1md <145151107+t1md@users.noreply.github.com>
2023-11-06 12:16:36 -0600

    save

--- 2d array/sketch.js
+++ 2d array/sketch.js
@@ -5,11 +5,41 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let grid = [[0,0,255,255,255],[255,0,0,255,0],[255,0,0,255,0],[255,255,0,0,255]];
+
+let numRows = 4;
+let numCols = 5;
+let rectWidth,rectHeight,row,col;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  rectWidth = 50;
+  rectHeight = 50;
+  createCanvas(numCols*rectWidth, numRows*rectHeight);
 }
 
 function draw() {
+  row = getCurY, col = getCurX;
   background(220);
+  drawGrid();
+}
+
+function getCurY(){
+  let consmouseX = constrain(mouseX,0,width);
+  return floor(consmouseX-1/rectWidth);
+}
+
+function getCurX(){
+  let consmouseY = constrain(mouseX, 0,height);
+  return floor(consmouseY-1/rectHeight);
+}
+
+function drawGrid(){
+  for(let x = 0;x<numCols;x++){
+    for(let y = 0; y<numRows;y++){
+      let fillValue = grid[y][x];
+      fill(fillValue);
+
+      rect(x*rectWidth,y*rectHeight,rectWidth,rectHeight);
+    }
+  }
 }
