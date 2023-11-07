88fc07d83612c47076eab083b11f04908540f946
t1md <145151107+t1md@users.noreply.github.com>
2023-11-07 12:54:14 -0600

    idk

--- 2d array/sketch.js
+++ 2d array/sketch.js
@@ -5,10 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let grid = [[0,0,255,255,255],[255,0,0,255,0],[255,0,0,255,0],[255,255,0,0,255]];
+let grid = [[0,0,255,0,255],[255,0,255,0,0],[255,255,0,255,0],[255,255,0,0,255]];
 
+let win = false;
 let numRows = 4;
 let numCols = 5;
+let found;
 let rectWidth,rectHeight,row,col;
 
 function setup() {
@@ -18,19 +20,20 @@ function setup() {
 }
 
 function draw() {
-  row = getCurY, col = getCurX;
+  row = getCurrentY(), col = getCurrentX();
   background(220);
   drawGrid();
+  print(win);
 }
 
-function getCurY(){
-  let consmouseX = constrain(mouseX,0,width);
-  return floor(consmouseX-1/rectWidth);
+function getCurrentX(){
+  let constrainMouseX = constrain(mouseX, 0, width-1);
+  return floor(constrainMouseX/rectWidth); 
 }
 
-function getCurX(){
-  let consmouseY = constrain(mouseX, 0,height);
-  return floor(consmouseY-1/rectHeight);
+function getCurrentY(){
+  let constrainMouseY = constrain(mouseY, 0, height-1);
+  return floor(constrainMouseY/rectHeight);
 }
 
 function drawGrid(){
@@ -41,5 +44,36 @@ function drawGrid(){
 
       rect(x*rectWidth,y*rectHeight,rectWidth,rectHeight);
     }
+  }
+  // winner();
+}
+
+function mouseClicked(){
+  if(keyIsPressed && keyCode===SHIFT){
+    flip(col,row);
+  }
+  else{
+    flip(col,row);
+    if(col<numCols-1){
+      flip(col+1,row);
+    }
+    if(col>0){
+      flip(col-1,row);
+    }
+    if(row<numRows-1){
+      flip(col,row+1);
+    }
+    if(row>0){
+      flip(col,row-1);
+    }
+  }
+}
+
+function flip(col,row){
+  if(grid[row][col]===0){
+    grid[row][col] = 255;
+  }
+  else{
+    grid[row][col] = 0;
   }
 }
