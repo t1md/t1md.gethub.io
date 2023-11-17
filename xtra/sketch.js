ea8546970837df0b42f98217d5caf50e38182394
t1md <145151107+t1md@users.noreply.github.com>
2023-11-17 12:16:18 -0600

    ?

--- xtra/sketch.js
+++ xtra/sketch.js
@@ -8,8 +8,25 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // dogTreats();
+  createFrame();
+}
+function dogTreats(){
+  let S = floor(random(11));
+  let M = floor(random(11));
+  let L = floor(random(11));
+  let happiness = 1*S + 2*M + 3*L;
+  print(S,M,L);
+  if(happiness>=10){
+    print("Barley is Happy");
+  }
+  else{
+    print("Barley is sad");
+  }
 }
 
-function draw() {
-  background(220);
+function createFrame(){
+  let N = floor(random(2,101));
+  for(let num = 0;num<N;num++){
+  }
 }
