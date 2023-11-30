93bdd79b20416523062816b84f23bc787b42a5b2
t1md <145151107+t1md@users.noreply.github.com>
2023-11-30 12:18:13 -0600

    start
    i began making the main parts of my game while playing around a bit with what different things  would do

--- major project/sketch.js
+++ major project/sketch.js
@@ -12,6 +12,8 @@ let grid =[];
 let numRows = 15;
 let numCols = 15;
 let rectWidth, rectHeight;
+let row,col;
+let defence = [];
 
 function setup() {
   for(let i = 0; i<numRows;i++){
@@ -19,21 +21,50 @@ function setup() {
   }
   rectWidth = 75;
   rectHeight = 75;
-  createCanvas(numCols*rectWidth, numRows*rectHeight);
+  createCanvas(numCols*rectWidth+200, numRows*rectHeight);
 }
 
 function draw() {
-  background(0);
+  row = getCurrentY();
+  col = getCurrentX();
+  background(255);
   base();
+  for(let d of defence){
+    d.create();
+  }
 }
 
 function base(){
-  stroke(color("skyblue"));
-  fill(0);
   for(let x = 0;x<numCols;x++){
     for(let y = 0; y<numRows;y++){
       rect(x*rectWidth,y*rectHeight,rectWidth,rectHeight);
     }
+  }
+}
+
+function getCurrentX(){
+  let constrainMouseX = constrain(mouseX, 0, width-1);
+  return floor(constrainMouseX/rectWidth); 
+}
+
+function getCurrentY(){
+  let constrainMouseY = constrain(mouseY, 0, height-1);
+  return floor(constrainMouseY/rectHeight);
+}
+
+function mousePressed(){
+  defence.push(new Tower(col,row,rectWidth));
+}
+
+class Tower{
+  constructor(x,y,s){
+    this.x = x*rectWidth+rectWidth/2;
+    this.y = y*rectHeight+rectHeight/2;
+    this.size = s;
+  }
+
+  create(){
+    circle(this.x,this.y,this.size);
   }
 }
 
