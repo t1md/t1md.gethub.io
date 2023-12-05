1b09c56e2e82c65eed00b92ff8debc6babdffef8
t1md <145151107+t1md@users.noreply.github.com>
2023-12-05 13:01:18 -0600

    final 1
    i started on bullets

--- major project/sketch.js
+++ major project/sketch.js
@@ -14,10 +14,11 @@ let numCols = 15;
 let rectWidth, rectHeight;
 let row,col;
 let defence = [];
+let bullet = [];
 
 function setup() {
   for(let i = 0; i<numRows;i++){
-    grid.push([]);
+    grid.push([0]);
   }
   rectWidth = 75;
   rectHeight = 75;
@@ -27,10 +28,10 @@ function setup() {
 function draw() {
   row = getCurrentY();
   col = getCurrentX();
-  background(255);
+  background(0,255,0);
   base();
   for(let d of defence){
-    d.create();
+    d.action();
   }
 }
 
@@ -43,7 +44,7 @@ function base(){
 }
 
 function getCurrentX(){
-  let constrainMouseX = constrain(mouseX, 0, width-1);
+  let constrainMouseX = constrain(mouseX, 0, numCols*rectWidth-1);
   return floor(constrainMouseX/rectWidth); 
 }
 
@@ -53,18 +54,53 @@ function getCurrentY(){
 }
 
 function mousePressed(){
-  defence.push(new Tower(col,row,rectWidth));
+  if(mouseX>0&&mouseX<numCols*rectWidth&&mouseY>0&&mouseY<height){
+    defence.push(new Tower(col,row));
+  }
 }
 
 class Tower{
-  constructor(x,y,s){
+  constructor(x,y){
     this.x = x*rectWidth+rectWidth/2;
     this.y = y*rectHeight+rectHeight/2;
-    this.size = s;
+    this.size = rectWidth;
+    this.fireRate = 1;
+
   }
 
-  create(){
+  createBase(){
     circle(this.x,this.y,this.size);
+    for(let b of bullet){
+      b.fire();
+    }
   }
+
+  shoot(){
+    bullet.push(new Bullet(this.x,this.y));
+  }
+
+  action(){
+    this.createBase();
+    this.fireRate+=1;
+    if(this.fireRate%15===0){ 
+      this.shoot();
+    }
+  }
+
 }
 
+class Bullet{
+  constructor(x,y){
+    this.position = createVector(x,y);
+    this.velocity = createVector(5,0);
+  }
+
+  fire(){
+    // if (this.position.x<0&&this.position.x>numCols*rectWidth&&
+    // this.position.y<0&&this.position.y>height){
+    //   bullet.splice(bullet.length-1,1);
+    // }
+    this.position.add(this.velocity);
+    circle(this.position.x,this.position.y,10);
+  }
+}
