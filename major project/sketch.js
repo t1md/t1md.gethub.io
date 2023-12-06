9c45776881d45e1615897a56713150c3a3eb7a9d
t1md <145151107+t1md@users.noreply.github.com>
2023-12-06 12:18:35 -0600

    final 2
    i worked on my bullets and fixed a problem where the bullets sped up as i placed more towers started on badguys

--- major project/sketch.js
+++ major project/sketch.js
@@ -8,21 +8,23 @@
 
 
 
-let grid =[];
 let numRows = 15;
 let numCols = 15;
-let rectWidth, rectHeight;
+let rectWidth = 75;
+let rectHeight =75;
 let row,col;
 let defence = [];
-let bullet = [];
+let grid = [];
+let enemies = [];
 
 function setup() {
-  for(let i = 0; i<numRows;i++){
-    grid.push([0]);
-  }
-  rectWidth = 75;
-  rectHeight = 75;
   createCanvas(numCols*rectWidth+200, numRows*rectHeight);
+  for (let i = 0; i < numRows; i++) {
+    grid.push(Array(numCols).fill(0));
+  }
+  for(let i = 0;i<10;i++){
+    enemies.push(new Enemy(0,10,5));
+  }
 }
 
 function draw() {
@@ -33,11 +35,14 @@ function draw() {
   for(let d of defence){
     d.action();
   }
+  for(let b of enemies){
+    b.action();
+  }
 }
 
 function base(){
-  for(let x = 0;x<numCols;x++){
-    for(let y = 0; y<numRows;y++){
+  for(let y = 0;y<numRows;y++){
+    for(let x = 0;x<numCols;x++){
       rect(x*rectWidth,y*rectHeight,rectWidth,rectHeight);
     }
   }
@@ -55,52 +60,104 @@ function getCurrentY(){
 
 function mousePressed(){
   if(mouseX>0&&mouseX<numCols*rectWidth&&mouseY>0&&mouseY<height){
-    defence.push(new Tower(col,row));
+    if(grid[col][row] === 0){
+      defence.push(new Tower(col,row));
+      grid[col][row] = 1;
+    }
   }
 }
 
 class Tower{
   constructor(x,y){
+    this.col = x;
+    this.row = y;
     this.x = x*rectWidth+rectWidth/2;
     this.y = y*rectHeight+rectHeight/2;
     this.size = rectWidth;
     this.fireRate = 1;
+    this.counter = 0;
+    this.bullets = [];
+  }
 
+  getColPosition(){
+    return this.col;
+  }
+  getRowPosition(){
+    return this.row;
   }
 
   createBase(){
     circle(this.x,this.y,this.size);
-    for(let b of bullet){
-      b.fire();
-    }
   }
 
-  shoot(){
-    bullet.push(new Bullet(this.x,this.y));
+  createBullet(){
+    this.bullets.push(new Bullet(this.x,this.y,-5,5));
+  }
+
+  bulletTravel(){
+    for(let b of this.bullets){
+      b.fire();
+      if(b.offscreen()){
+        this.bullets.splice(b,1);
+      }
+    }
   }
 
   action(){
     this.createBase();
-    this.fireRate+=1;
-    if(this.fireRate%15===0){ 
-      this.shoot();
+    this.counter+=1;
+    if(this.counter%(this.fireRate*60)===0){ 
+      this.createBullet();
     }
+    this.bulletTravel();
   }
 
 }
 
 class Bullet{
-  constructor(x,y){
+  constructor(x,y,speed){
     this.position = createVector(x,y);
-    this.velocity = createVector(5,0);
+    this.bulletSpeed = createVector(speed,0);
+  }
+
+  createBase(){
+    circle(this.position.x,this.position.y,10);
+  }
+
+  movement(){
+    this.position.add(this.bulletSpeed);
   }
 
   fire(){
-    // if (this.position.x<0&&this.position.x>numCols*rectWidth&&
-    // this.position.y<0&&this.position.y>height){
-    //   bullet.splice(bullet.length-1,1);
-    // }
-    this.position.add(this.velocity);
-    circle(this.position.x,this.position.y,10);
+    this.createBase();
+    this.movement();
+  }
+
+  offscreen(){
+    if(this.position.x > numCols*rectWidth || this.position.x < 0 || this.position.y > height || this.position.y < 0){
+      return true;
+    }
+  }
+}
+
+class Enemy{
+  constructor(x,y){
+    this.position = createVector(x,y*rectHeight+rectHeight/2);
+    this.travelSpeed = createVector(5,0);
+  }
+
+  movement(){
+    this.position.add(this.travelSpeed);
+  }
+
+  createEnemy(){
+    fill("red");
+    circle(this.position.x,this.position.y,25);
+    fill(255);
+  }
+
+  action(){
+    this.createEnemy();
+    this.movement();
   }
 }
